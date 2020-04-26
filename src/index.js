8953b8d7e15ec7ea2b45b2969b65e78fd96b9576
Harish Karthik <hhkk0010@outlook.com>
2020-04-26 22:30:55 +0530

    Added redux and related files

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
 import {BrowserRouter} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 const appRouter = (
-    <BrowserRouter>
-        <App/>
-    </BrowserRouter>
+    <Provider>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </Provider>
 )
 
 ReactDOM.render(appRouter, document.getElementById('root'));
---
+++ src/stores/reducers.js
@@ -0,0 +1,12 @@
+import * as actionTypes from './actions'
+
+const initialState = {
+    ingredients : null,
+    totalPrice : 4
+}
+
+const reducer = (state,action) => {
+    return state
+}
+
+export default reducer;
