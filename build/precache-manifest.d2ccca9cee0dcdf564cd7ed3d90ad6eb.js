8aa12f8dab54177a61b8f53feb475f47c9e003c9
Harish Karthik <hhkk0010@outlook.com>
2020-05-09 12:48:57 +0530

    Built the code for deployment

---
+++ build/precache-manifest.d2ccca9cee0dcdf564cd7ed3d90ad6eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32326153474848bcfcf50a92b0d183a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4700c91d88add50dc5b",
+    "url": "/static/css/main.507922fe.chunk.css"
+  },
+  {
+    "revision": "4f1e51423322fae58853",
+    "url": "/static/js/2.e18b77f5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e18b77f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4700c91d88add50dc5b",
+    "url": "/static/js/main.0eeb59a6.chunk.js"
+  },
+  {
+    "revision": "0a76d1f20b9a1baca5a6",
+    "url": "/static/js/runtime-main.c86b15f3.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/burger.b8503d26.png"
+  }
+]);
